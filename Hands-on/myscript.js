5446d9c20cb6a7a2eabc55cedb7a84c7c7dd3965
Siva OS <syskantechnosoft@users.noreply.github.com>
2022-08-17 16:22:56 -0400

    Add files via upload

---
+++ Hands-on/myscript.js
@@ -0,0 +1,5 @@
+document.write("Welcome to JavaScript!!!!");
+//alert("This is JS Alert box!!!");
+//prompt("Enter any number...");
+
+console.log("this will printed in the console only!!");
---
+++ Hands-on/script1.js
@@ -0,0 +1,63 @@
+//creating variables
+
+//method1 using var keyword
+
+var x; // variable declaration (declaring a variable before using it)
+
+x=25; // assigning a value to the variable
+
+document.write("x = "+x);
+
+
+var y = 40; //variable declaration and initialization 
+
+document.write("   &nbsp; &nbsp; y=" + y);
+
+
+//method2 using let keyword
+
+let dob = "12/03/1983";  // declared and assigned value to a variable (string type)
+
+let notNumber = "78"; //string data type
+
+document.write("  dob =" + dob);
+
+document.write ("  notNumber =" + notNumber);
+
+document.write ( "  " + typeof(x));
+document.write ("  " + typeof(y));
+document.write ( "  " +typeof(dob));
+document.write ( "  " +typeof(notNumber));
+
+x=90;
+y=75;
+
+dob = 20;
+notNumber =  'z';
+
+document.write("x = "+x);
+
+document.write("   &nbsp; &nbsp; y=" + y);
+
+document.write("  dob =" + dob);
+
+document.write ("  notNumber =" + notNumber);
+
+document.write ( "  " + typeof(x));
+document.write ("  " + typeof(y));
+document.write ( "  " +typeof(dob));
+document.write ( "  " +typeof(notNumber));
+
+
+//method 3 declaring variable without keywords
+
+a=75;
+b=false;
+c='e';
+d="Name";
+
+ x = 16 + "Volvo";
+
+
+document.write("x = " +x);
+
